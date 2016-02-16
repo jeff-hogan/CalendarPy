4d5b93a1bf84b2e832f849e9cb266e48e70cd98a
Jeff Hogan <jeff.hogan@nebraska.gov>
2016-02-16 13:03:55 -0600

    day font scailing working

--- CalendarPy.py
+++ CalendarPy.py
@@ -12,6 +12,23 @@ def MonthText(Month):
     months =["January","February","March","April","May","June", "July","August","September","October","November","December"]
     return months[Month]
 
+def MaxFont(draw,text,rect):
+    i=1
+    font=ImageFont.truetype("arial.ttf",i)
+    while (draw.textsize(text,font)[0]<rect[2]-rect[0] and
+           draw.textsize(text,font)[1]<rect[3]-rect[1]):
+        i+=1
+        font=ImageFont.truetype("arial.ttf",i)
+        #print(i)
+    font=ImageFont.truetype("arial.ttf",i-2)
+    return font
+
+def GetCentered(draw,text,font,rect):
+    W=rect[2]-rect[0]
+    H=rect[3]-rect[1]
+    w,h=draw.textsize(text,font)
+    return ( rect[0]+(W-w)//2,rect[1]+(H-h)//2 )
+
 def Calendar(year,month):
     dpi = 300
     
@@ -21,7 +38,7 @@ def Calendar(year,month):
 
     cal=Image.new("RGB",(Width,Height),"white")
     draw=ImageDraw.Draw(cal)
-
+    
     Header = Height//5
     HeaderCellWidth=(Width-2*Margin)//7
     HeaderCellHeight =(Height-2*Margin)//20
@@ -30,7 +47,16 @@ def Calendar(year,month):
     PrevSmallCalendarRect = (2*Margin, Margin, Header + Margin - HeaderCellHeight, Header + Margin - HeaderCellHeight)
     smallPrevBegin=(2*Margin,Margin)
     smallPrev=calendar.TextCalendar(6).formatmonth(year,month-1)
-    font=ImageFont.truetype("arial.ttf",20)
+
+    #i=1
+    #font=ImageFont.truetype("arial.ttf",i)
+    #while (draw.textsize(smallPrev,font)[0]<PrevSmallCalendarRect[2]-PrevSmallCalendarRect[0] and
+    #       draw.textsize(smallPrev,font)[1]<PrevSmallCalendarRect[3]-PrevSmallCalendarRect[1]):
+    #    i+=1
+    #    font=ImageFont.truetype("arial.ttf",i)
+    #    print(i)
+    #font=ImageFont.truetype("arial.ttf",i-1)
+    font=MaxFont(draw,smallPrev,PrevSmallCalendarRect)
     draw.text(smallPrevBegin,smallPrev,(0,0,0),font=font)
 
     #Draw small next month
@@ -41,10 +67,14 @@ def Calendar(year,month):
     draw.text(smallNextBegin,smallPrev,(0,0,0),font=font)
 
 
+    DaysLabelRect = (Margin + HeaderCellWidth * 3, Header + Margin - HeaderCellHeight, Margin + HeaderCellWidth * 3 + HeaderCellWidth, Header + Margin - HeaderCellHeight + HeaderCellHeight)
+    font=MaxFont(draw,DayText(3),DaysLabelRect)
     for Day in range(7):
         DaysLabelRect = (Margin + HeaderCellWidth * Day, Header + Margin - HeaderCellHeight, Margin + HeaderCellWidth * Day + HeaderCellWidth, Header + Margin - HeaderCellHeight + HeaderCellHeight)
         draw.rectangle(DaysLabelRect,outline=(0,0,0))
-        draw.text(DaysLabelRect,DayText(Day),(0,0,0),font=font)
+        #draw.text(DaysLabelRect,DayText(Day),(0,0,0),font=font)
+        draw.text(GetCentered(draw,DayText(Day),font,DaysLabelRect),DayText(Day),(0,0,0),font=font)
+        
 
 
     Days=calendar.Calendar(6).monthdayscalendar(year,month)
@@ -65,8 +95,10 @@ def Calendar(year,month):
 
 
     #Draw Header
+    HeaderText=MonthText(month)+" " + str(year)
     HeaderRect = (Margin, Margin, Margin + Width - 2 * Margin, Margin + Header - HeaderCellHeight)
-    draw.text(HeaderRect,MonthText(month)+" " + str(year),(0,0,0),font=font)
+    font=MaxFont(draw,HeaderText,HeaderRect)
+    draw.text(HeaderRect,HeaderText,(0,0,0),font=font)
 
 
 
